1e5c00a3a0069ad28b49f66bd44d21eea9a3dfc8
MaxwellJacobins <negativekarma1@gmail.com>
2017-03-11 07:29:41 -0800

    Update Vec2.js

--- Vec2.js
+++ Vec2.js
@@ -13,6 +13,14 @@ Vec.prototype.set = function(vec)
 {
 	this.x = vec.x
 	this.y = vec.y
+	return this
+}
+
+Vec.prototype.reverse = function()
+{
+	this.x *= -1
+	this.y *= -1
+	return this
 }
 
 Vec.prototype.add = function(vec)
@@ -62,8 +70,29 @@ Vec.prototype.getMag = function()
 	return Math.sqrt(this.x * this.x + this.y * this.y)
 }
 
+Vec.prototype.getAngle = function()
+{
+	return Math.atan2(this.y, this.x)
+}
+
 Vec.prototype.getNorm = function()
 {
 	var mag = this.getMag()
 	return new Vec(this.x/mag, this.y/mag)
+}
+
+Vec.prototype.setMag = function(toMag)
+{
+	var angle = this.getAngle()
+	this.x = Math.cos(angle) * toMag
+	this.y = Math.sin(angle) * toMag
+	return this
+}
+
+Vec.prototype.setAngle = function(toAngle)
+{
+	var mag = this.getMag()
+	this.x = Math.cos(toAngle) * mag
+	this.y = Math.sin(toAngle) * mag
+	return this
 }
