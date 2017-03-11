0326f39cb5291a88e4c7831ba0bed28d74520a4a
MaxwellJacobins <negativekarma1@gmail.com>
2017-03-10 20:03:43 -0800

    Update Vec2.js

--- Vec2.js
+++ Vec2.js
@@ -28,3 +28,42 @@ Vec.prototype.sub = function(vec)
 	this.y -= vec.y
 	return this
 }
+
+Vec.prototype.mult = function(val)
+{
+	if (val instanceof Vec)
+	{
+		this.x *= val.x
+		this.y *= val.y
+	} else
+	{
+		this.x *= fac
+		this.y *= fac
+	}
+	return this
+}
+
+Vec.prototype.div = function(val)
+{
+	if (val instanceof Vec)
+	{
+		this.x /= val.x
+		this.y /= val.y
+	} else
+	{
+		this.x /= fac
+		this.y /= fac
+	}
+	return this
+}
+
+Vec.prototype.getMag = function()
+{
+	return Math.sqrt(this.x * this.x + this.y * this.y)
+}
+
+Vec.prototype.getNorm = function()
+{
+	var mag = this.getMag()
+	return new Vec(this.x/mag, this.y/mag)
+}
